bf64f27eb41c7f2fb8b356744e9bede9aa503edd
Timo770 <tbriand@iu.edu>
2026-09-21 20:46:00 -0400

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,15 @@
+const number1 = Number(prompt("enter number1"));
+const number2 = Number(prompt("enter number2"));
+const number3 = Number(prompt("enter number3"));
+const average = (number1 + number2  + number3) / 3;
+console.log(average);
+
+
+const name = "Timo";
+const city = "Indianapolis";
+const sport = prompt(name + " lives in " + city + ". What is your favorite sport?");
+console.log(name + " " + "likes" + " " + sport + " " + "and lives in" + " " + city + ".");
+const day = prompt("What day it is?");
+console.log(day);
+const time = prompt("What time is it");
+console.log(time);
